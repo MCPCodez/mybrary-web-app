4445edbed5a6e1f400c8197ea2b4b771ddfd6044
MCPCodez <tidaldecode@gmail.com>
2024-04-21 06:24:00 -0500

    initial author browse

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ https://youtu.be/qj2oDkvc4dQ?list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM
 
 I would also like to thank Web Dev Simplified for all the hard work they put in thank you and I will be doing reviews of each project after I complete in this learning journey.# mybrary-web-app
 # mybrary-web-app
+
+Notes below for self
+
+If creating async routes remember all the syntax and remember to change your ejs exmaple in partials folder in errorMessage.ejs. author.js is the route file if you want to see the async function for that or you can use the code underneath synchronously either way works.
--- routes/author.js
+++ routes/author.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const Author = require('../models/author');
 
 // All Authors Route
-router.get('/',(req,res,next)=>{
+router.get('/', async (req,res,next)=>{
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index',{ authors: authors,
+                                     searchOptions: req.query })
+    } catch {
+        res.redirect('/');
+    }
     res.render('authors/index');
 });
 
@@ -13,23 +24,33 @@ router.get('/new',(req,res,next)=>{
 });
 
 // Post route to create 
-router.post('/',(req,res,next)=>{
+router.post('/', async (req,res,next)=>{
     // Create a new author 
     const author = new Author({
         name: req.body.name
     });
     // Save the new author that was created
-    author.save()
-        .then(function(newAuthor){
-            //res.redirect(`authors/${newAuthor.id}`);
-            res.redirect(`authors`);
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res .redirect(`authors`)
+    } catch {
+        res.render('authors/new',{
+            author:author,
+            errorMessage: 'Error creating author!'
         })
-        .catch(function(err){
-            res.render('authors/new',{
-                author:author,
-                errorMessage: 'Error creating author!'
-            })
-        })
+    }
+    //author.save()
+    //    .then(function(newAuthor){
+    //        //res.redirect(`authors/${newAuthor.id}`);
+    //        res.redirect(`authors`);
+    //    })
+    //    .catch(function(err){
+    //        res.render('authors/new',{
+    //           author:author,
+    //            errorMessage: 'Error creating author!'
+    //        })
+    //    })
 });
 
 module.exports = router;
